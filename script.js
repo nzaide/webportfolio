aec6ad6065267457023d265661503103dbb245e1
norozaide <norozaide@gmail.com>
2022-06-09 12:24:49 +0800

    updated the carousel

--- script.js
+++ script.js
@@ -35,8 +35,6 @@ function toggleState3() {
 
 let imgObject = [
   "img/capstone1a-card.png",
-  "img/capstone1b-card.png",
-  "img/capstone1c-card.png",
   "img/capstone2card.png",
   "img/try.png"
 ];
